ce88b05a36ca312d50ea0a83aa9de2fb9e4021c0
eyern <eyern254@gmail.com>
2025-03-22 11:08:00 +0300

    csrf

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-8bc0f.up.railway.app', 'https://web-production-8bc0f.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['web-production-8bc0f.up.railway.app', 'https://web-production-8bc0f.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-8bc0f.up.railway.app']
 
 
 # Application definition
